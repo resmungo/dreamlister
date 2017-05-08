34ef70ea8d175f2f86f5896e09c4d8c31f88d64b
Allan Edwards <aedwards@emich.edu>
2017-05-08 09:20:22 -0400

    Initial Commit

---
+++ DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Allan Edwards on 5/8/17.
+//  Copyright © 2017 Allan Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
